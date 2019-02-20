20fcb7c2e54d8920c7177c71da34b1c8fcafd122
Shubham Sinha <35834780+s-sinha9@users.noreply.github.com>
2019-02-21 01:47:57 +0530

    Required files for qrscanner activity

---
+++ MainActivity.java
@@ -0,0 +1,71 @@
+package shubham.system.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import android.content.Intent;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+//implementing onclicklistener
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //View Objects
+    private Button buttonScan;
+    private TextView textViewHeader, textViewAddress;
+
+    //qr code scanner object
+    private IntentIntegrator qrScan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //View objects
+        buttonScan = (Button) findViewById(R.id.buttonScan);
+        textViewHeader = (TextView) findViewById(R.id.textViewHeader);
+        textViewAddress = (TextView) findViewById(R.id.textViewAddress);
+
+        //intializing scan object
+        qrScan = new IntentIntegrator(this);
+
+        //attaching onclick listener
+        buttonScan.setOnClickListener(this);
+    }
+
+    //Getting the scan results
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            //if qrcode has nothing in it
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            } else {
+                //if qr contains data
+                textViewHeader.setText("Scan to Exit");
+                String str=result.getContents();
+                str= "In time is "+ str;
+                textViewAddress.setText(str);
+
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+    @Override
+    public void onClick(View view) {
+        //initiating the qr code scan
+        qrScan.initiateScan();
+    }
+}
